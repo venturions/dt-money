079e4956ef3e1de569a633d7f8aa75ae4ecc8b97
Alex Costa <alex.costa@bitfy.app>
2023-05-28 22:55:07 -0300

    projeto utilizado no modulo 3 do ignite

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,76 @@
+import { ReactNode, useEffect, useState, useCallback } from "react";
+import { api } from "../lib/axios";
+import { createContext } from "use-context-selector";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>;
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType);
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface CreateTransactionInput {
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+}
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const fetchTransactions = useCallback(async (query?: string) => {
+    const response = await api.get("/transactions", {
+      params: {
+        _sort: "created_at",
+        _order: "desc",
+        q: query,
+      },
+    });
+
+    setTransactions(response.data);
+  }, []);
+
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data;
+
+      const response = await api.post("/transactions", {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      });
+
+      setTransactions((state) => [response.data, ...state]);
+    },
+    []
+  );
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [fetchTransactions]);
+
+  return (
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransaction }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
